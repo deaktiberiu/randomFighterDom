2ae46ee94e6eee028b41e87ffb8ad84304f8ff94
deakTiberiu <deaktiberiu93@gmail.com>
2022-03-23 17:09:10 +0200

    created inital game mechanichs

--- README.md
+++ README.md
@@ -10,4 +10,13 @@ TODO
 
 3 - Display player attributes - DONE
 
-4 - Crete simple game mechanics
+4 - Create game mechanics:
+_ i) create attack - Done
+_ ii) create random damage - DONE
+_ iii) calculate if defender dodges the attack - DONE
+_ iv) if defender dodges switch sides and def becomes attacker (repeat) -DONE
+_ v) if defender does not dodge calculate the damage and reduce from health -DONE
+_ vi) if healt <= 0 attacker wins -DONE
+\_ vii) if healt is >0 switch attacker with defender DONE
+
+5 - Create delay between attacks
--- mechanics.js
+++ mechanics.js
@@ -3,8 +3,8 @@ const plOne = {
   plName: "playerOne",
   plStatus: 1,
   hp: 200,
-  attDmg: 2000,
-  dgC: 10,
+  attDmg: 30,
+  dgC: 0.05,
 };
 
 const plTwo = {
@@ -12,10 +12,59 @@ const plTwo = {
   plStatus: 0,
   hp: 100,
   attDmg: 10,
-  dgC: 50,
+  dgC: 0.5,
 };
 
+let attackPlayer = plOne;
+let defendingPlayer = plTwo;
+
+// game mechanics
+// switch players
+function switchPlayers() {
+  if (plOne.hp > 0 && plTwo.hp > 0) {
+    console.error(`attack`);
+
+    if (attackPlayer.plName === plOne.plName) {
+      defendingPlayer = plTwo;
+      defendingPlayer = plOne;
+    } else {
+      attackPlayer = plOne;
+      defendingPlayer = plTwo;
+    }
+
+    setTimeout(attack(attackPlayer, defendingPlayer), 10000);
+  } else {
+    console.error(`game over`);
+  }
+}
+
+// generate dmg
+const generateDmg = (dmg) => Math.round(Math.random() * dmg);
+
+// see if the defender dodged the attack
+const askIfHit = (dodge) => (Math.random() < dodge ? true : false);
+
+// attack function
+function attack(attacker, defender) {
+  const dmg = generateDmg(attacker.attDmg);
+  const doesDodge = askIfHit(defender.dgC);
+  console.warn(`${attacker.plName} is active and his dmg is ${dmg} `);
+  if (!doesDodge) {
+    console.error(`${defender.plName} does NOT dodge the attack`);
+    defender.hp -= dmg;
+    console.warn(
+      `player ${attacker.plName} does ${dmg} to ${defender.plName} (${defender.hp} left)`
+    );
+  } else {
+    console.error(`${defender.plName} does DOES dodge the attack`);
+  }
+
+  switchPlayers();
+}
+
+// create ui
 function displayPlayers(player) {
+  console.log(player);
   const pl = document.getElementById(player.plName);
 
   pl.querySelector(`.name span`).innerHTML = player.plName;
@@ -28,6 +77,7 @@ function displayPlayers(player) {
 function startGame() {
   displayPlayers(plOne);
   displayPlayers(plTwo);
+  attack(attackPlayer, defendingPlayer);
 }
 
 startGame();
